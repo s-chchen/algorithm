81c1b5f56d968e24ffc16e0a7a46b158c44b0950
s-chchen <1843093222@qq.com>
2023-01-21 23:01:39 +0800

    timu6 两个数组的交集

--- src/leetbook/junior/timu6.java
+++ src/leetbook/junior/timu6.java
@@ -3,13 +3,13 @@ package leetbook.junior;
 /*
 两个数组的交集
 
-给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。
+返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。
+可以不考虑输出结果的顺序。
 
 * */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class timu6 {
 
@@ -39,5 +39,44 @@ public class timu6 {
     }
 
     /*Map*/
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        int len1=nums1.length;
+        int len2=nums2.length;
+
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<len1;i++){
+            if(!map.containsKey(nums1[i])){
+                map.put(nums1[i],1);
+            }else{
+                int val=map.get(nums1[i]);
+                map.put(nums1[i],++val);
+            }
+        }
+
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<len2;i++){
+            if(map.containsKey(nums2[i])){
+                int val=map.get(nums2[i]);
+                if(val>0){
+                    list.add(nums2[i]);
+                    map.put(nums2[i],--val);
+                }
+            }
+        }
+
+        int[] res=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            res[i]=list.get(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1={4,9,5};
+        int[] nums2={9,4,9,8,4};
+        int[] res=intersect2(nums1,nums2);
+        Arrays.stream(res).forEach(System.out::println);
+    }
 
 }
